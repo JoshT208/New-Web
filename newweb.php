36c94b70a6ad31f1e3b0df2d7f90e7432f7ecc3a
EvanLeroy <128870446+EvanLeroy@users.noreply.github.com>
2023-03-31 20:17:06 -0400

    Update and rename newweb.html to newweb.php

--- newweb.php
+++ newweb.php
@@ -4,6 +4,21 @@
     <title>Processing.JS inside Webpages: Template</title> 
   </head>
   <body>
+	  <button></button>
+	  <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
 	<!--This draws the canvas on the webpage -->
     <canvas id="mycanvas"></canvas> 
   </body>
@@ -12,6 +27,25 @@
   <!-- See https://khanacademy.zendesk.com/hc/en-us/articles/202260404-What-parts-of-ProcessingJS-does-Khan-Academy-support- for differences -->
   <script src="https://cdn.jsdelivr.net/processing.js/1.4.8/processing.min.js"></script> 
   <script>
+	 /*
+var connection = new ActiveXObject("ADODB.Connection") ;
+
+var connectionstring="Data Source=sql9.freesqldatabase.com;Initial Catalog=<catalog>;User ID=sql9609690;Password=s24BhEihFV;Provider=SQLOLEDB";
+
+connection.Open(connectionstring);
+var rs = new ActiveXObject("ADODB.Recordset");
+
+rs.Open("SELECT * FROM table", connection);
+rs.MoveFirst
+while(!rs.eof)
+{
+   document.write(rs.fields(1));
+   rs.movenext;
+}
+*/
+	  
+rs.close;
+connection.close; 
   var programCode = function(processingInstance) {
     with (processingInstance) {
       size(400, 400); 
